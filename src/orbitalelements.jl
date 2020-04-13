82afeb2edcfc97062c1cb3dd11552750e660f56c
jared711 <jared711@stanford.edu>
2020-04-13 16:23:21 -0700

    jldoctest added to orbitalelements.jl

--- src/orbitalelements.jl
+++ src/orbitalelements.jl
@@ -3,11 +3,11 @@
 
 Compute the semimajor axis given apoapsis and periapsis distances rA and rP.
 
-#Example
+# Example
 ```jldoctest
 julia> AP2a(1e6,1e5)
 550000.0
-
+```
 """
 function AP2a(rA, rP)
     a = (rA + rP)/2
