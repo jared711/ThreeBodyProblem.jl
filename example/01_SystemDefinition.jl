71f27f88e6d739d20ba4cb7aae8285a2e26e99e1
Jared Blanchard <jared711@stanford.edu>
2022-10-14 15:48:40 -0700

    Updating exmples and appending 0 to names

---
+++ example/01_SystemDefinition.jl
@@ -0,0 +1,90 @@
+@time using ThreeBodyProblem
+
+## Defining New Bodies
+
+### We use the Body() constructor to create a new planet and a new moon
+
+# Create the ice planet Hoth
+m = 1e24 # {kg} mass
+R = 1e6 # {km} mean radius
+a = 1.5*AU # {km} mean semimajor axis
+T = 0.6*JY # {sec} mean orbital period
+name = "Hoth"
+color = :blue
+newplanet = Body(m, R, a, T, name, color)
+
+# Create the forest moon Endor
+m = 1e22 # {kg} mass
+R = 1e6 # {km} mean radiusasdf
+a = 0.2*AU # {km} mean semimajor axis
+T = 2π*sqrt(a^3/(G*newplanet.m)) # {sec} mean orbital period
+name = "Endor"
+color = :green
+newmoon = Body(m, R, a, T, name, color)
+
+### Note that we can use the constants AU, JY, JD, and G to define our bodies
+
+println("AU = ",AU) # km in an astronomical unit
+println("JY = ",JY) # seconds in a Julian Year
+println("JD = ",JD) # seconds in a Julian day
+println("G = ",G)  # universal gravitational constant
+
+## Defining a New System
+
+### We use the System() constructor with newplanet and newmoon as our primary and secondary bodies.
+
+sys = System(newplanet, newmoon)
+
+### We can access system parameters with the dot operator, including the body structs in the system
+
+println("System period is ",sys.T, " seconds")
+println("The Primary Body is ", sys.prim.name)
+
+### In the CR3BP, the dynamics of a system are completely defined by its mass parameter, μ.
+
+sys.μ
+
+### While it looks pretty to use greek letters μ and other unicode operators like superscripts r¹ and subscripts r₁, they may cause trouble with plotting and in other situations. So there are always non-unicode alternatives, like sys.mu instead of sys.μ
+
+sys.mu
+
+## Plotting a System
+
+### We can plot a System object using the plot() command
+
+using Plots
+plot(sys)
+
+# Notice that the plot is made in the rotating frame, with non-dimensionalized coordinates
+
+###  We can also plot individual bodies
+
+plot(newmoon)
+
+# Note that bodies plotted alone will be centered at the origin and be plotted in dimensional coordinates
+
+### We can plot just the bodies in the CR3BP by setting Lpts to false, or vice versa
+
+plot(sys, Lpts=false)
+
+plot(sys, prim=false, sec=false)
+
+### There are pre-programmed variables and functions for many common systems.
+
+println("EARTH = ",EARTH)
+println("SUN = ",SUN)
+println("MOON = ",MOON)
+println("JUPITER = ",JUPITER)
+println("EUROPA = ",EUROPA)
+println("STYX = ",STYX)
+sys = System(SUN,MERCURY)
+
+plot(jupiter_europa())
+
+# When you plot these real systems, it's interesting to see the relative size between the primary and secondary
+
+plot(saturn_enceladus())
+
+### You can set the 'scaled' argument to 'true' to make the bodies easier to see, but note that they are not their true size
+
+plot(saturn_enceladus(),scaled=true)
--- example/02_EarthMoonTraj.jl
+++ example/02_EarthMoonTraj.jl
@@ -1,4 +1,4 @@
-using ThreeBodyProblem
+@time using ThreeBodyProblem
 using OrdinaryDiffEq
 using Plots
 pyplot() # This command sets pyplot as our plotting backend
@@ -14,7 +14,7 @@ L1 = computeL1(p) # calculate the Lagrange point L1 for the given system paramet
 Lpts = computeLpts(p)
 L4 = Lpts[4]
 
-R₁,R₂ = computeR1R2(p) # {km} distances of Primary and Secondary bodies from origin
+R₁,R₂ = computed1d2(p) # {km} distances of Primary and Secondary bodies from origin
 ωₛ = sqrt((μ₁ + μ₂)/d^3) # {rad/s} rotation rate of system
 
 
@@ -37,7 +37,7 @@ vᵢ = 10.92367104    # {km/s} injection velocity in rotating frame
 ϕ = 47.70061087     # {°} injection angle, measured from +y
 
 
-r₀ = [-R₁ - (Rₑ + 200)*cosd(ϕ); -(Rₑ + 200)*sind(ϕ); 0]
+r₀ = [R₁ - (Rₑ + 200)*cosd(ϕ); -(Rₑ + 200)*sind(ϕ); 0]
 v₀ = vᵢ*[sind(ϕ); -cosd(ϕ); 0];
 rv₀ = [r₀;v₀] # {km; km/s} our initial state
 
@@ -45,10 +45,10 @@ tspan = (0.,86400*6.) # {sec} 6 day span
 
 
 prob = ODEProblem(CR3BPdynamics!,rv₀,tspan,p) # CR3BPdynamics! is our in-place dynamics function
-sol = solve(prob,reltol=1e-6)
+sol = solve(prob,Tsit5(),reltol=1e-6)
 
 plot(sol,vars=(1,2),title="Lunar return trajectory in rotating frame")
-plot!(circle(Rₑ,[-R₁;0]),color="blue",label="Earth")
+plot!(circle(Rₑ,[R₁;0]),color="blue",label="Earth")
 plot!(circle(Rₘ,[R₂;0]),color="gray",label="Moon",aspect_ratio=:equal)
 
 rv_inert = [rot2inert(sol.u[i],sol.t[i],p) for i = 1:length(sol)]
